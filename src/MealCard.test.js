62a142c4600350dbfd47fdf6c01685c8bfcc214d
Amanda <ajnikrant@gmail.com>
2021-07-16 15:03:03 -0400

    basic unit tests built

--- src/MealCard.test.js
+++ src/MealCard.test.js
@@ -1,9 +1,32 @@
-import { render, screen } from '@testing-library/react';
+import React, { createElement } from 'react';
+import ReactDom from 'react-dom';
+import { render } from '@testing-library/react';
 import MealCard from './MealCard';
 
+const mockMeal = {
+    name: "Test Meal",
+    image: "someURL",
+    price: 1, 
+    description: "test desc",
+    ingredients: "test ingredients"
+  };
 
+  const mockSearchEntry = "test"
+  
+  // **************** Test one ****************
+test('Meal Card component matches snapshot', () => {
+    const { asFragment } = render(<MealCard meal={mockMeal} allergenSearch={mockSearchEntry} />);
 
-test ('Fake Test', ()=> {
-    expect(2+2).toBe(4)
-})
+    expect(asFragment()).toMatchSnapshot();
+});
+
+// **************** Test two ****************
+test('Card renders without crashing', () => {
+
+    const div = document.createElement('div');
+
+    render(
+        <MealCard meal={mockMeal} allergenSearch={mockSearchEntry} />, div
+    );
+});
 
